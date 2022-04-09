70858963747fcc1494da2aec569ef38c819c7727
Chelly321 <michelle.royer@icloud.com>
2022-04-09 16:24:29 -0400

    class-commit

---
+++ opdracht1.js
@@ -0,0 +1,19 @@
+import (inventory);
+
+// Opdracht 1a: Gebruik een array-methode om een array te maken met alle tv-type namen.
+// Log de uitkomst in de console.
+// const tvNames = ('4K TV', 'HD smart TV', '4K QLED TV', 'Ultra HD SMART TV', 'The One 4K TV', '4K LED TV', 'LED TV', 'HD TV');
+
+
+// Opdracht 1b: Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die volledig uitverkocht zijn.
+// Log de uitkomst in de console.
+// if sold is equal to original stock == sold out
+
+
+// Opdracht 1c: Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die over AmbiLight beschikken.
+// Log de uitkomst in de console.
+
+
+
+// Opdracht 1d: Schrijf een functie die alle tv's van laagste naar hoogste prijs sorteert.
+// Log de uitkomst in de console.
